71941d3ec442c876561a6e6183e29825f2dcbb5c
Akmiru <105554789+Akmiru@users.noreply.github.com>
2023-03-11 17:18:14 +0100

    First changings

--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -8,13 +8,13 @@ import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class CommandExample extends Command {
     public CommandExample() {
-        super("example", "Sends a message.");
+        super("authors", "Sends a message.");
     }
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
-            info("hi");
+            info("Lipur and Mewok129");
             return SINGLE_SUCCESS;
         });
     }
